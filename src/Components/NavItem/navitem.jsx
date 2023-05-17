3c96615957e6103f6bea6bc62aa7a460487819ec
SanC005 <82254558+ReiZero1349@users.noreply.github.com>
2023-05-17 16:32:46 +0530

    added navbar

---
+++ src/Components/NavItem/navitem.jsx
@@ -0,0 +1,8 @@
+
+export default function NavItem(){
+    return(
+        <div className="h-12 w-1/5 bg-indigo-500">
+            <h1>hey</h1>
+        </div>
+    )
+}
---
+++ src/Components/Navbar/navbar.jsx
@@ -0,0 +1,8 @@
+import NavItem from "../NavItem/navitem"
+export default function Navbar(){
+    return(
+        <div className="flex h-12 w-full bg-black">
+            <NavItem />
+        </div>
+    )
+}
