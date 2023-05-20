af9e1da8ac0677c4bd1951521f153ca31dae7e46
SanC005 <82254558+ReiZero1349@users.noreply.github.com>
2023-05-20 12:11:27 +0530

    fixed texts in boxes

--- src/Components/Box/box.jsx
+++ src/Components/Box/box.jsx
@@ -2,11 +2,11 @@
 export default function Box({data}){
     // const item = props.
     return(
-        <div className="w-64 h-64 bg-blue-300 mx-4 my-8">
-            <div className="text-center align-middle p-2 border-2 border-black">
+        <div className="w-64 h-64 bg-blue-300 mx-4 my-8 overflow-hidden text-ellipsis">
+            <div className="text-center align-middle p-2 bg-blue-400">
                 {data.title}
             </div>
-            <div className="text-center align-middle p-2 border-2 border-black">
+            <div className="text-center align-middle p-5 flex-nowrap">
                 {data.body}
                 
             </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,20 @@
 
 import Navbar from "@/Components/Navbar/navBar"
 import Row from "@/Components/Row/row"
+// const dlist = [
+//     { id: 1,title: "hey" },
+//     { id: 2,title: "hey" },
+//     { id:3 ,title: "hey" },
+//     { id: 4,title: "hey" },
+//     { id: 5,title: "hey" },
+//     { id: 6,title: "hey" },
+//     { id: 7,title: "hey" },
+//     { id:8 ,title: "hey" },
+//     { id: 9,title: "hey" },
+//     { id:10 ,title: "hey" },
+//     { id: 11,title: "hey" },
+//     { id: 12,title: "hey" },
+// ];
 export default function Home() {
   
   
